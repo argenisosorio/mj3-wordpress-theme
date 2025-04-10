f4d8144f3696ee712716d51e1833d1d3b5935981
Argenis Osorio <argenisosorio580@hotmail.com>
2025-04-10 15:29:14 -0400

    Ajustes de estilos

--- patterns/banner-hero.php
+++ patterns/banner-hero.php
@@ -13,14 +13,35 @@
 	style="border: ridge 1px blue;"
 >
     <div class="py-lg-3 position-relative">
-        <div class="row text-center">
+        <div class="row text-center" style="min-height: 350px;">
             <div class="col-2">
             </div>
-            <div class="col-4" style="border: ridge 1px red;">
-                FFFFF
-            </div>
-            <div class="col-4" style="border: ridge 1px red;">
-                XXXXX
+            <div class="col-8" style="border: ridge 1px transparent;">
+                <div
+                    class="card"
+                    style="
+                        margin-top: 50px;
+                        background-color: #18181894; border: none;
+                        color: white;
+                    "
+                >
+                    <div class="card-body">
+                        <h5 class="card-title" style="color: white;">
+                            <b>COMPANY NAME</b>
+                        </h5>
+                        <p class="card-text">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            <br>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        </p>
+                        <a href="#">
+                            <button class="btn btn-secondary btn-grad mt-3 mb-3">
+                                <b>ABOUT</b>
+                            </button>
+                        </a>
+                    </div>
+                </div>
             </div>
             <div class="col-2">
             </div>
