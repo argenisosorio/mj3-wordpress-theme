c8e29fa581f9adb61c0d233ee7da325a4a9c3b26
Argenis Osorio <argenisosorio580@hotmail.com>
2025-04-21 21:24:30 -0400

    Update media

--- patterns/banner-hero.php
+++ patterns/banner-hero.php
@@ -17,7 +17,7 @@
             <source
                 src="
                     <?php echo esc_url( get_template_directory_uri() );
-                    ?>/assets/videos/video.webm" alt="<?php esc_attr_e( '',
+                    ?>/assets/videos/video2.mp4" alt="<?php esc_attr_e( '',
                     'twentytwentyfour' );
                     ?>
                 "
