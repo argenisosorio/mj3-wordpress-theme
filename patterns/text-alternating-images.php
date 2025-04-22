0f1141555faee3262581acb32b36c1be689cf488
Argenis Osorio <argenisosorio580@hotmail.com>
2025-04-22 11:17:28 -0400

    Update texts and styles

--- patterns/text-alternating-images.php
+++ patterns/text-alternating-images.php
@@ -13,108 +13,336 @@
         class="container "
         style="border: ridge 1px transparent;"
     >
-        <h1 class="text-center font-6" style="color: orangered;">
+        <h1 class="text-center font-6" style="color: #df2528;">
             <b>SERVICES</b>
         </h1>
         <br>
-        <!-- Columna 1 -->
+        <!-- Fila 1 -->
         <div
             class="row"
             style="border: ridge 1px transparent;"
         >
             <div class="col-4" style="border: ridge 1px transparent;">
-                <div
-                    class="card section2"
-                    style="
-                        /*background-color: #18181894;*/
-                        border: ridge 1px transparent;
-                        color: #000000;
-                    "
-                >
-                    <div class="card-body font-6">
-                        <h2 class="font-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                <div class="card text-white card-services">
+                    <img
+                        class="section3 img-services"
+                        style="width: 100%; height: 100%;"
+                        src="
+                            <?php echo esc_url( get_template_directory_uri() );
+                            ?>/assets/images/room_images/1.webp" alt="<?php esc_attr_e( '',
+                            'twentytwentyfour' );
+                            ?>
+                        "
+                        alt="image"
+                    >
+                    <div class="card-img-overlay font-6">
+                        <h5 class="card-title text-white">Roofing Construction and Repair</h5>
+                        <p class="card-text">
+                            Installation, maintenance, and repair of all types
+                            of roofs (tiles, sheet metal, flat) with
+                            high-strength materials and guaranteed watertightness.
                         </p>
                     </div>
                 </div>
             </div>
             <div class="col-4" style="border: ridge 1px transparent;">
-                <img
-                    class="img-zoom section3"
-                    style="width: 100%;"
-                    src="
-                        <?php echo esc_url( get_template_directory_uri() );
-                        ?>/assets/images/room_images/1.webp" alt="<?php esc_attr_e( '',
-                        'twentytwentyfour' );
-                        ?>
-                    "
-                    alt="image"
-                >
+                <div class="card text-white card-services">
+                    <img
+                        class="section3 img-services"
+                        style="width: 100%; height: 100%;"
+                        src="
+                            <?php echo esc_url( get_template_directory_uri() );
+                            ?>/assets/images/room_images/1.webp" alt="<?php esc_attr_e( '',
+                            'twentytwentyfour' );
+                            ?>
+                        "
+                        alt="image"
+                    >
+                    <div class="card-img-overlay font-6">
+                        <h5 class="card-title text-white">Flooring Construction and Repair</h5>
+                        <p class="card-text">
+                            Professional installation of wood, ceramic,
+                            porcelain, laminate, and polished concrete flooring
+                            for interiors and exteriors.
+                        </p>
+                    </div>
+                </div>
             </div>
             <div class="col-4" style="border: ridge 1px transparent;">
-                <img
-                    class="img-zoom section2"
-                    style="width: 100%;"
-                    src="
-                        <?php echo esc_url( get_template_directory_uri() );
-                        ?>/assets/images/room_images/2.webp" alt="<?php esc_attr_e( '',
-                        'twentytwentyfour' );
-                        ?>
-                    "
-                    alt="image"
-                >
+                <div class="card text-white card-services">
+                    <img
+                        class="section3 img-services"
+                        style="width: 100%; height: 100%;"
+                        src="
+                            <?php echo esc_url( get_template_directory_uri() );
+                            ?>/assets/images/room_images/1.webp" alt="<?php esc_attr_e( '',
+                            'twentytwentyfour' );
+                            ?>
+                        "
+                        alt="image"
+                    >
+                    <div class="card-img-overlay font-6">
+                        <h5 class="card-title text-white">Kitchens and Bathrooms</h5>
+                        <p class="card-text">
+                            Comprehensive design and remodeling with premium
+                            materials, including granite, custom finishes, and
+                            space optimization.
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
-        <!-- Final Columna 1 -->
+        <!-- Final Fila 1 -->
         <br>
-        <!-- Columna 2 -->
+        <!-- Fila 2 -->
         <div
             class="row"
             style="border: ridge 1px transparent;"
         >
             <div class="col-4" style="border: ridge 1px transparent;">
-                <img
-                    class="img-zoom section3"
-                    style="width: 100%;"
-                    src="
-                        <?php echo esc_url( get_template_directory_uri() );
-                        ?>/assets/images/room_images/3.webp" alt="<?php esc_attr_e( '',
-                        'twentytwentyfour' );
-                        ?>
-                    "
-                    alt="image"
-                >
+                <div class="card text-white card-services">
+                    <img
+                        class="section3 img-services"
+                        style="width: 100%; height: 100%;"
+                        src="
+                            <?php echo esc_url( get_template_directory_uri() );
+                            ?>/assets/images/room_images/1.webp" alt="<?php esc_attr_e( '',
+                            'twentytwentyfour' );
+                            ?>
+                        "
+                        alt="image"
+                    >
+                    <div class="card-img-overlay font-6">
+                        <h5 class="card-title text-white">Fencing</h5>
+                        <p class="card-text">
+                            Installation and repair of wood, metal, and vinyl
+                            fences for security and privacy.
+                        </p>
+                    </div>
+                </div>
             </div>
             <div class="col-4" style="border: ridge 1px transparent;">
-                <img
-                    class="img-zoom section2"
-                    style="width: 100%;"
-                    src="
-                        <?php echo esc_url( get_template_directory_uri() );
-                        ?>/assets/images/room_images/4.webp" alt="<?php esc_attr_e( '',
-                        'twentytwentyfour' );
-                        ?>
-                    "
-                    alt="image"
-                >
+                <div class="card text-white card-services">
+                    <img
+                        class="section3 img-services"
+                        style="width: 100%; height: 100%;"
+                        src="
+                            <?php echo esc_url( get_template_directory_uri() );
+                            ?>/assets/images/room_images/1.webp" alt="<?php esc_attr_e( '',
+                            'twentytwentyfour' );
+                            ?>
+                        "
+                        alt="image"
+                    >
+                    <div class="card-img-overlay font-6">
+                        <h5 class="card-title text-white">Concrete</h5>
+                        <p class="card-text">
+                            Construction of floors, sidewalks, patios, and
+                            concrete structures using durable techniques.
+                        </p>
+                    </div>
+                </div>
             </div>
             <div class="col-4" style="border: ridge 1px transparent;">
-                <img
-                    class="img-zoom section3"
-                    style="width: 100%;"
-                    src="
-                        <?php echo esc_url( get_template_directory_uri() );
-                        ?>/assets/images/room_images/4.webp" alt="<?php esc_attr_e( '',
-                        'twentytwentyfour' );
-                        ?>
-                    "
-                    alt="image"
-                >
+                <div class="card text-white card-services">
+                    <img
+                        class="section3 img-services"
+                        style="width: 100%; height: 100%;"
+                        src="
+                            <?php echo esc_url( get_template_directory_uri() );
+                            ?>/assets/images/room_images/1.webp" alt="<?php esc_attr_e( '',
+                            'twentytwentyfour' );
+                            ?>
+                        "
+                        alt="image"
+                    >
+                    <div class="card-img-overlay font-6">
+                        <h5 class="card-title text-white">Extensions</h5>
+                        <p class="card-text">
+                            We add living spaces to your property, from bedrooms
+                            to second floors.
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
-        <!-- Final Columna 2 -->
+        <!-- Final Fila 2 -->
+        <br>
+        <!-- Fila 3 -->
+        <div
+            class="row"
+            style="border: ridge 1px transparent;"
+        >
+            <div class="col-4" style="border: ridge 1px transparent;">
+                <div class="card text-white card-services">
+                    <img
+                        class="section3 img-services"
+                        style="width: 100%; height: 100%;"
+                        src="
+                            <?php echo esc_url( get_template_directory_uri() );
+                            ?>/assets/images/room_images/1.webp" alt="<?php esc_attr_e( '',
+                            'twentytwentyfour' );
+                            ?>
+                        "
+                        alt="image"
+                    >
+                    <div class="card-img-overlay font-6">
+                        <h5 class="card-title text-white">Sheetrock (Drywall)</h5>
+                        <p class="card-text">
+                            Installation and repair of walls and ceilings with
+                            durable materials and impeccable finishes.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4" style="border: ridge 1px transparent;">
+                <div class="card text-white card-services">
+                    <img
+                        class="section3 img-services"
+                        style="width: 100%; height: 100%;"
+                        src="
+                            <?php echo esc_url( get_template_directory_uri() );
+                            ?>/assets/images/room_images/1.webp" alt="<?php esc_attr_e( '',
+                            'twentytwentyfour' );
+                            ?>
+                        "
+                        alt="image"
+                    >
+                    <div class="card-img-overlay font-6">
+                        <h5 class="card-title text-white">Electrical Services</h5>
+                        <p class="card-text">
+                            Wiring, lighting, panels, and safe and efficient energy solutions.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4" style="border: ridge 1px transparent;">
+                <div class="card text-white card-services">
+                    <img
+                        class="section3 img-services"
+                        style="width: 100%; height: 100%;"
+                        src="
+                            <?php echo esc_url( get_template_directory_uri() );
+                            ?>/assets/images/room_images/1.webp" alt="<?php esc_attr_e( '',
+                            'twentytwentyfour' );
+                            ?>
+                        "
+                        alt="image"
+                    >
+                    <div class="card-img-overlay font-6">
+                        <h5 class="card-title text-white">Plumbing Services</h5>
+                        <p class="card-text">
+                            Comprehensive installation, maintenance, and repair
+                            of drinking water, drainage, gas, and sanitary systems.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Final Fila 3 -->
+        <br>
+        <!-- Fila 4 -->
+        <div
+            class="row"
+            style="border: ridge 1px transparent;"
+        >
+            <div class="col-4" style="border: ridge 1px transparent;">
+                <div class="card text-white card-services">
+                    <img
+                        class="section3 img-services"
+                        style="width: 100%; height: 100%;"
+                        src="
+                            <?php echo esc_url( get_template_directory_uri() );
+                            ?>/assets/images/room_images/1.webp" alt="<?php esc_attr_e( '',
+                            'twentytwentyfour' );
+                            ?>
+                        "
+                        alt="image"
+                    >
+                    <div class="card-img-overlay font-6">
+                        <h5 class="card-title text-white">Granite</h5>
+                        <p class="card-text">
+                            Granite countertops and surfaces for kitchens,
+                            bathrooms, and common areas.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4" style="border: ridge 1px transparent;">
+                <div class="card text-white card-services">
+                    <img
+                        class="section3 img-services"
+                        style="width: 100%; height: 100%;"
+                        src="
+                            <?php echo esc_url( get_template_directory_uri() );
+                            ?>/assets/images/room_images/1.webp" alt="<?php esc_attr_e( '',
+                            'twentytwentyfour' );
+                            ?>
+                        "
+                        alt="image"
+                    >
+                    <div class="card-img-overlay font-6">
+                        <h5 class="card-title text-white">Tree Services</h5>
+                        <p class="card-text">
+                            Professional pruning, controlled felling, and
+                            maintenance of landscaping with specialized equipment.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4" style="border: ridge 1px transparent;">
+                <div class="card text-white card-services">
+                    <img
+                        class="section3 img-services"
+                        style="width: 100%; height: 100%;"
+                        src="
+                            <?php echo esc_url( get_template_directory_uri() );
+                            ?>/assets/images/room_images/1.webp" alt="<?php esc_attr_e( '',
+                            'twentytwentyfour' );
+                            ?>
+                        "
+                        alt="image"
+                    >
+                    <div class="card-img-overlay font-6">
+                        <h5 class="card-title text-white">General Painting</h5>
+                        <p class="card-text">
+                            Interior/exterior painting with professional
+                            techniques and high-quality materials.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Final Fila 4 -->
+        <br>
+        <!-- Fila 5 -->
+        <div
+            class="row"
+            style="border: ridge 1px transparent;"
+        >
+            <div class="col-4" style="border: ridge 1px transparent;">
+                <div class="card text-white card-services">
+                    <img
+                        class="section3 img-services"
+                        style="width: 100%; height: 100%;"
+                        src="
+                            <?php echo esc_url( get_template_directory_uri() );
+                            ?>/assets/images/room_images/1.webp" alt="<?php esc_attr_e( '',
+                            'twentytwentyfour' );
+                            ?>
+                        "
+                        alt="image"
+                    >
+                    <div class="card-img-overlay font-6">
+                        <h5 class="card-title text-white">A/C and Climate Control</h5>
+                        <p class="card-text">
+                            Installation and repair of air conditioning,
+                            ventilation, and heating systems.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Final Fila 5 -->
     </div>
 </div>
